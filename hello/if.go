acdaf99c42b45763320f2c5e00fc85d67d044ad9
aarora0301 <aarora0301@gmail.com>
2020-01-29 11:33:39 +0530

    understanding if flow

---
+++ hello/if.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+
+var input int
+fmt.Println("enter number")
+fmt.Scan(&input)
+
+if input%2==0 {
+
+fmt.Println("input is even")
+
+}else {
+
+ fmt.Println("input is odd")
+}
+
+}
